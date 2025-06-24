9e505668ea82806aa935c55dcfa0fc4768f2644e
PriyankaKharde <priyankakharde9@gmail.com>
2025-06-24 13:59:08 +0530

    first changes

--- python_3.py
+++ python_3.py
@@ -1,1 +1,13 @@
 
+import streamlit as st
+import pandas as pd
+ 
+st.write("""
+# My first app
+Hello *world!*
+""")
+ 
+df = pd.read_csv("my_data.csv")
+st.line_chart(df)
+
+file = st.file_uploader("Pick a file")
